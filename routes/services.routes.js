048d34bacb907d96a4f626b8a207586a0ede79b9
ZiggyHub <chapu_11torres@hotmail.com>
2024-02-21 16:45:14 -0600

    Se egregan las carpetas middlewares y validators que contienen sus respectivos archivos para validaciones de userPost

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const { serviceGet, servicePost, servicePut, serviceDel } = require('../controllers/services.controller');
+const { celebrateValidator } = require('../middlewares/celebrateValidator');
 
 const router = Router();
 
-router.post("/", servicePost);
+router.post("/", celebrateValidator, servicePost);
 router.get("/", serviceGet);
 router.put("/:id", servicePut);
 router.delete("/:id", serviceDel);
---
+++ validators/users.validators.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+    userName: Joi.string().min(3).max(30).required().messages({
+        "string.base":`Username debe de ser una cadena de caracteres`,
+        "string.empty":`Username no debe de estar vacio`,
+        "string.min":`Username debe tener un minimo de {$limit} caracteres`,
+        "any.required":`El campo username es requerido`
+    }),
+    email: Joi.string().email().required().messages({
+        "stringemail":`El campo debe de tener un formato valido`
+    }),
+    password: Joi.string().min(3).max(30).required().messages({
+        "string.min": "El campo password debe de tener un minimo de {#limit} caracteres",
+    }),
+   state: Joi.boolean()
+})
+
+module.exports = (schema)
