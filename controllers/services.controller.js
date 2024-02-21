a83e38584743278147879d01d0da27b5afcc0b06
ZiggyHub <chapu_11torres@hotmail.com>
2024-02-21 14:46:52 -0600

    Se agregan las rutas-controladores servicePut(V) y serviceDel(v)

--- controllers/services.controller.js
+++ controllers/services.controller.js
@@ -39,7 +39,45 @@ try {
 
 }
 
+const servicePut = async( req, res ) => {
+    try {
+        const { id } = req.params;
+        const { name, active, price } = req.body
+        const data = { name, active, price };
+
+        await Service.findByIdAndUpdate(id, data)
+
+        res.status(200).json({
+            message:'Servicio actualizado con exito',
+            ok:true
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
+}
+ 
+const serviceDel = async (req, res) =>{
+    try {
+        const { id } = req.params
+        const falseActive = {active: false}
+        await Service.findByIdAndUpdate(id, falseActive )
+  
+        res.status(200).json({
+          message: `El servicio ${id} fue eliminado`
+        })
+  
+    } catch (error) {
+        res.status(200).json({
+            message: `Algo salio mal al intentar eliminar el servicio ${id}`,
+                service
+            })
+    }
+}
 module.exports = {
     serviceGet,
-    servicePost
+    servicePost,
+    servicePut,
+    serviceDel
 }
--- routes/services.routes.js
+++ routes/services.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { serviceGet, servicePost } = require('../controllers/services.controller');
+const { serviceGet, servicePost, servicePut, serviceDel } = require('../controllers/services.controller');
 
 const router = Router();
 
 router.post("/", servicePost);
 router.get("/", serviceGet);
+router.put("/:id", servicePut);
+router.delete("/:id", serviceDel);
 
 
 module.exports = router
